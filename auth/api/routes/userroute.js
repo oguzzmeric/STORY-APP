52ef4a814cca12f8bd5abfcefeaebc6762f2fc5e
oğuz umut <oguzmeric9@gmail.com>
2024-11-19 16:34:29 +0300

    Removed unnecessary folders

--- auth/api/routes/userroute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {test} from '../controller/usercontroller.js'
-
-const router = express.Router();
-
-router.get('/',test)
-
-export default router;
--- bookd/backend/config.js
+++
@@ -1,2 +0,0 @@
-export const PORT  = 5000 ;
-export const mongoDBURL = 'mongodb+srv://oguzmeric9:oguz@books-mern.d4g8xb3.mongodb.net/?retryWrites=true&w=majority&appName=books-mern'
